b902c6c9db5e8836fb9e49942ab4cd1c0c965bc8
Adam Tetz <mail@adamtetz.nl>
2025-06-30 14:55:28 +0200

    random enum choice function.

--- integrationsandbox/TMS/factories.py
+++ integrationsandbox/TMS/factories.py
@@ -1,6 +1,13 @@
+import random
+from enum import Enum
+
 from faker import Faker
 
-from integrationsandbox.TMS.models import TmsCustomer, TmsLineItem
+from integrationsandbox.TMS.models import PackageType, TmsCustomer, TmsLineItem
+
+
+def get_random_enum_choice(enum_attribute: Enum) -> Enum:
+    return random.choice(list(enum_attribute))
 
 
 class TmsShipmentFactory:
@@ -14,13 +21,21 @@ class TmsShipmentFactory:
             name=fake.company(),
             carrier=f"{fake.company()} Transport",
         )
-    
-    def create_line_item(self) -> TmsLineItem
+
+    def create_line_item(self) -> TmsLineItem:
+        fake = self.fake
+        return TmsLineItem(
+            id=fake.uuid4(),
+            name=fake.company(),
+            carrier=f"{fake.company()} Transport",
+        )
 
 
 def main() -> None:
     shipment_factory = TmsShipmentFactory()
-    print(shipment_factory.create_customer())
+    customer = shipment_factory.create_customer()
+
+    print(get_random_enum_choice(PackageType))
 
 
 if __name__ == "__main__":
