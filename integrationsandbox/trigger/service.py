a592ca04489756f28325119748a0c9bac1ae6656
Adam Tetz <mail@adamtetz.nl>
2025-07-27 19:26:47 +0200

    Fixed bug in create_and_dispatch_events

--- integrationsandbox/trigger/service.py
+++ integrationsandbox/trigger/service.py
@@ -4,6 +4,7 @@ import httpx
 
 from integrationsandbox.broker.models import BrokerEventMessage
 from integrationsandbox.broker.service import build_events, create_events
+from integrationsandbox.common.exceptions import ValidationError
 from integrationsandbox.tms.models import TmsShipment
 from integrationsandbox.tms.service import (
     build_shipments,
@@ -35,13 +36,9 @@ def dispatch_events_to_url(events: List[BrokerEventMessage], url: str) -> None:
 
 
 def create_and_dispatch_events(trigger: EventTrigger) -> List[BrokerEventMessage]:
-    try:
-        shipments = get_shipments_by_id_list(trigger.shipment_ids)
-    except ValueError:
-        # If some/all shipments don't exist, continue with empty list
-        # This allows trigger to be resilient for testing scenarios
-        shipments = []
-
+    if not trigger.shipment_ids:
+        raise ValidationError("No shipment_ids received.")
+    shipments = get_shipments_by_id_list(trigger.shipment_ids)
     events = build_events(shipments, trigger.event)
     create_events(events)
     dispatch_events_to_url(events, trigger.target_url.encoded_string())
